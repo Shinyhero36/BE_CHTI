2f20283d7e6059f5a7cffb25a0b1dc0391c0913b
Jean-Yves Saint-Loubert <saint-lo@insa-toulouse.fr>
2023-04-11 15:41:34 +0200

    Initial commit for StepSon

---
+++ Src/principal.c
@@ -0,0 +1,50 @@
+#include "DriverJeuLaser.h"
+
+
+extern int LongueurSon, TableIndex;
+extern short Son, SortieSon;
+//CallbackSon Fonction C et ASM
+void CallbackSon(void);
+void CallbackSon_VertionC(){
+	if (LongueurSon > TableIndex){
+		int NoteSon = (&Son)[TableIndex];
+		TableIndex++;
+		NoteSon += 32768;
+		NoteSon /= 91;
+		SortieSon = NoteSon;
+		PWM_Set_Value_TIM3_Ch3(NoteSon);
+	}
+}
+
+
+
+int main(void)
+{
+
+// ===========================================================================
+// ============= INIT PERIPH (faites qu'une seule fois)  =====================
+// ===========================================================================
+	
+// Après exécution : le coeur CPU est clocké à 72MHz ainsi que tous les timers
+CLOCK_Configure();
+	
+// configuration de PortB.1 (PB1) en sortie push-pull
+GPIO_Configure(GPIOB, 1, OUTPUT, OUTPUT_PPULL);
+	
+// configuration du Timer 3 pour la PWM fréquencé à 0.1 GHz (72M / 720)
+GPIO_Configure(GPIOB, 0, OUTPUT, ALT_PPULL);
+PWM_Init_ff( TIM3, 3, 720);
+
+// configuration du Timer 4 en débordement 100ms
+Timer_1234_Init_ff( TIM4, 6552);
+Active_IT_Debordement_Timer( TIM4, 2, CallbackSon);
+
+
+//============================================================================	
+	
+	
+while	(1)
+	{
+	}
+}
+
